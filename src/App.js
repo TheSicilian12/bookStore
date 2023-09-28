9a3e2dfac5accdf3c2bcf672ad68bde77f91c689
TheSicilian12 <mguidera314@gmail.com>
2023-09-28 13:20:10 -0400

    adjusting style to have a drop down

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
 
   return (
     <div>
-      <div className="flex flex-col text-center">
+      <div className="flex flex-col">
         <div className="font-bold text-7xl">
           Jan's
         </div>
@@ -30,8 +30,10 @@ function App() {
           Family Book Store
         </div>
       </div>
-      <div className="flex justify-center mt-0.5">
-        <div className="top-28 left-0 w-16 m-0 w-9/12">
+
+
+      <div className="flex justify-around mt-0.5 border-solid border-t-2 border-b-2 border-black">
+        {/* <div className="top-28 left-0 w-16 m-0 w-9/12">
           <div className="flex border-solid border-t-2 border-b-2 border-black">
             <i className="sidebar-icon">NOVELS</i>
             <i className="sidebar-icon">ROMANCE</i>
@@ -39,27 +41,33 @@ function App() {
             <i className="sidebar-icon">WESTERN</i>
             <i className="sidebar-icon">SCIENCE FICTION</i>
           </div>
+        </div> */}
+
+        <div>
+          <div>
+            <div>Hours</div>
+            <div>Monday: 11 AM - 5 PM</div>
+            <div>Tuesday: 11 AM - 5 PM</div>
+            <div>Wednesday: 11 AM - 3:30 PM</div>
+            <div>Thursday: 11 AM - 3:30 PM</div>
+            <div>Friday: 11 AM - 3:30 PM</div>
+            <div> Saturday: 10 AM - 3:30 PM</div>
+            <div>Sunday: Closed</div>
+          </div>
         </div>
 
         <div>
-          Hours
-          <div>Monday: 11 AM - 5 PM</div>
-          <div>Tuesday: 11 AM - 5 PM</div>
-          <div>Wednesday: 11 AM - 3:30 PM</div>
-          <div>Thursday: 11 AM - 3:30 PM</div>
-          <div>Friday: 11 AM - 3:30 PM</div>
-          <div> Saturday: 10 AM - 3:30 PM</div>
-          <div>Sunday: Closed</div>
+          <div>
+            Location
+            <div>309 N Cole St Lima, OH 45805</div>
+          </div>
         </div>
 
         <div>
-          Location
-          <div>309 N Cole St Lima, OH 45805</div>
-        </div>
-
-        <div>
-          Contact
-          <div>(567) 204-2718</div>
+          <div>
+            Contact
+            <div>(567) 204-2718</div>
+          </div>
         </div>
 
 
@@ -71,4 +79,4 @@ function App() {
 export default App;
 
 // https://www.npmjs.com/package/react-image-gallery
-{/* <ImageGallery items={images} /> */}
+{/* <ImageGallery items={images} /> */ }
