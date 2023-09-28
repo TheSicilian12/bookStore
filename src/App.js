eb8e4194afc35f9e61dfb0a43d8711f01b95cedc
TheSicilian12 <mguidera314@gmail.com>
2023-09-28 11:27:44 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import './input.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React hello
-        </a>
-      </header>
+    <div>
+      <div className="flex flex-col text-center">
+        <div className="font-bold text-7xl">
+          Jan's
+        </div>
+        <div className="font-bold text-xl">
+          Family Book Store
+        </div>
+      </div>
+      <div className="flex justify-center mt-0.5">
+        <div className="top-28 left-0 w-16 m-0 w-9/12">
+          <div className="flex border-solid border-t-2 border-b-2 border-black">
+            <i className="sidebar-icon">NOVELS</i>
+            <i className="sidebar-icon">ROMANCE</i>
+            <i className="sidebar-icon">MYSTERIES</i>
+            <i className="sidebar-icon">WESTERN</i>
+            <i className="sidebar-icon">SCIENCE FICTION</i>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
