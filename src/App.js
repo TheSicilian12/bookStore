6c27d1f74653b24eb3af3b08e59481d365ac4ca7
TheSicilian12 <mguidera314@gmail.com>
2023-09-28 11:41:32 -0400

    added initial slider

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import ImageGallery from "react-image-gallery";
+
 import './App.css';
 import './input.css';
 
 function App() {
+  const images = [
+    {
+      original: "https://picsum.photos/id/1018/1000/600/",
+      thumbnail: "https://picsum.photos/id/1018/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1015/1000/600/",
+      thumbnail: "https://picsum.photos/id/1015/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1019/1000/600/",
+      thumbnail: "https://picsum.photos/id/1019/250/150/",
+    },
+  ];
+
   return (
     <div>
       <div className="flex flex-col text-center">
@@ -23,9 +40,22 @@ function App() {
             <i className="sidebar-icon">SCIENCE FICTION</i>
           </div>
         </div>
+
+        <ImageGallery items={images} />
       </div>
+
+
+
+
+
+
+
+
+
     </div>
   );
 }
 
 export default App;
+
+// https://www.npmjs.com/package/react-image-gallery
