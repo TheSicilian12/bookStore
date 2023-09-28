58cd00b9c45d708e148cc261d007a9949e5a5093
TheSicilian12 <mguidera314@gmail.com>
2023-09-28 13:35:35 -0400

    setInfoDisplay changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ImageGallery from "react-image-gallery";
 
 import './App.css';
 import './input.css';
 
 function App() {
-  const images = [
-    {
-      original: "https://picsum.photos/id/1018/1000/600/",
-      thumbnail: "https://picsum.photos/id/1018/250/150/",
-    },
-    {
-      original: "https://picsum.photos/id/1015/1000/600/",
-      thumbnail: "https://picsum.photos/id/1015/250/150/",
-    },
-    {
-      original: "https://picsum.photos/id/1019/1000/600/",
-      thumbnail: "https://picsum.photos/id/1019/250/150/",
-    },
-  ];
+  const [infoDisplay, setInfoDisplay] = useState(true);
 
   return (
     <div>
@@ -31,39 +18,37 @@ function App() {
         </div>
       </div>
 
+      <div>
+        <i className="fa fa-angle-down"
+          onClick={() => setInfoDisplay(true)}></i>
+        <i className="fa fa-angle-up"
+          onClick={() => setInfoDisplay(false)}></i>
 
-      <div className="flex justify-around mt-0.5 border-solid border-t-2 border-b-2 border-black">
-        {/* <div className="top-28 left-0 w-16 m-0 w-9/12">
-          <div className="flex border-solid border-t-2 border-b-2 border-black">
-            <i className="sidebar-icon">NOVELS</i>
-            <i className="sidebar-icon">ROMANCE</i>
-            <i className="sidebar-icon">MYSTERIES</i>
-            <i className="sidebar-icon">WESTERN</i>
-            <i className="sidebar-icon">SCIENCE FICTION</i>
-          </div>
-        </div> */}
+      {infoDisplay && <div>test</div>}
 
-        <div>
+        <div className="flex justify-around mt-0.5 border-solid border-t-2 border-b-2 border-black">
           <div>
-            <div className="font-bold text-4xl">Hours</div>
-            <div>Monday & Tuesday: 11 AM - 5 PM</div>
-            <div>Wednesday - Friday: 11 AM - 3:30 PM</div>
-            <div> Saturday: 10 AM - 3:30 PM</div>
-            <div>Sunday: Closed</div>
+            <div>
+              <div className="font-bold text-4xl">Hours</div>
+              <div>Monday & Tuesday: 11 AM - 5 PM</div>
+              <div>Wednesday - Friday: 11 AM - 3:30 PM</div>
+              <div> Saturday: 10 AM - 3:30 PM</div>
+              <div>Sunday: Closed</div>
+            </div>
           </div>
-        </div>
 
-        <div>
           <div>
-          <div className="font-bold text-4xl">Location</div>
-            <div>309 N Cole St Lima, OH 45805</div>
+            <div>
+              <div className="font-bold text-4xl">Location</div>
+              <div>309 N Cole St Lima, OH 45805</div>
+            </div>
           </div>
-        </div>
 
-        <div>
           <div>
-          <div className="font-bold text-4xl">Contact</div>
-            <div>(567) 204-2718</div>
+            <div>
+              <div className="font-bold text-4xl">Contact</div>
+              <div>(567) 204-2718</div>
+            </div>
           </div>
         </div>
 
