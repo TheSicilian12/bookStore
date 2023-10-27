b6f4c40f4432dd3b488d512a278ee4f1cfb46225
TheSicilian12 <mguidera314@gmail.com>
2023-10-27 16:44:16 -0400

    working on background image

--- src/App.js
+++ src/App.js
@@ -120,7 +120,11 @@ function App() {
         <img className="w-screen backgroung-img"
           src={background} alt="background"
         ></img>
-        <div className='background-text'>Your Community Book Store</div>
+        <div className='background-text-container'>
+          <div className='background-image-text'>
+            Your Community Book Store
+          </div>
+          </div>
       </div>
 
       <div className={`${rotate === "rotate-down" ? 'info-small-container' : 'info-container'} flex justify-around background-info`}>
